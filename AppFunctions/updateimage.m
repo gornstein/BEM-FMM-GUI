8f10ec3c535b529ea1870e9ce4d1b1af5fbdf7ba
saphireraider <96030083+saphireraider@users.noreply.github.com>
2022-05-09 17:35:11 -0400

    Combined both Alton's and Kseniia's code changes

--- AppFunctions/updateimage.m
+++ AppFunctions/updateimage.m
@@ -1,6 +1,20 @@
 function [] = updateimage(app)
 %%  The main function called to update the UIAxes (with the coil and brain)
 
+%   Rounds all of the fields in the matrix
+app.MatrixField11.Value = round(app.MatrixField11.Value, 10);
+app.MatrixField12.Value = round(app.MatrixField12.Value, 10);
+app.MatrixField13.Value = round(app.MatrixField13.Value, 10);
+app.MatrixField21.Value = round(app.MatrixField21.Value, 10);
+app.MatrixField22.Value = round(app.MatrixField22.Value, 10);
+app.MatrixField23.Value = round(app.MatrixField23.Value, 10);
+app.MatrixField31.Value = round(app.MatrixField31.Value, 10);
+app.MatrixField32.Value = round(app.MatrixField32.Value, 10);
+app.MatrixField33.Value = round(app.MatrixField33.Value, 10);
+app.MatrixField41.Value = round(app.MatrixField41.Value, 10);
+app.MatrixField42.Value = round(app.MatrixField42.Value, 10);
+app.MatrixField43.Value = round(app.MatrixField43.Value, 10);
+
 cla(app.UIAxes); %clears the axis
 hold(app.UIAxes, "on"); %ensures that both will be plotted
 
