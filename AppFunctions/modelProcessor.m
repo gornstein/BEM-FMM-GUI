df7c85145ec0a2fb870c8a9605907e8677843785
kr0man0va <67516645+kr0man0va@users.noreply.github.com>
2022-06-03 20:08:55 -0400

    _
    _

---
+++ AppFunctions/modelProcessor.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = modelProcessor(inputArg1,inputArg2)
+%MODELPROCESSOR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
