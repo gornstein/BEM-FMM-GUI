ea74f5505bd9e17ffeaa961dc00f2846f1fa5cbc
saphireraider <96030083+saphireraider@users.noreply.github.com>
2022-06-04 12:34:36 -0400

    Prelim stuff (nothing working)

--- AppFunctions/pointClickUpdateFunction.m
+++
@@ -1,7 +0,0 @@
-function txt = pointClickUpdateFunction(~,event_obj)
-%   ~           Currently not used (empty)
-%   event_obj   Object containing event data structure
-%   txt         Displayed text when clicking on the point
-pos = get(event_obj, 'Position');
-txt = '';
-end
