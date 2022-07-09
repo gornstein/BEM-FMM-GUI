0eed207c5b7117c18bae742ffe93e97e2b37b96c
saphireraider <96030083+saphireraider@users.noreply.github.com>
2022-07-09 15:03:02 -0400

    Updated the lines to conform to the new units and changed the location of the normalization of the direction vectors to the plotting

--- GUIEngine/displayobslines.m
+++ GUIEngine/displayobslines.m
@@ -5,13 +5,23 @@ if (length(app.lines{app.selectedlineidx}) == 5)
 end
 
 %   Sets all of the variables
-startingXCoord = app.lines{app.selectedlineidx}{3}(1);
-startingYCoord = app.lines{app.selectedlineidx}{3}(2);
-startingZCoord = app.lines{app.selectedlineidx}{3}(3);
-magnitude = app.lines{app.selectedlineidx}{2}(4);
-endingXCoord = startingXCoord + app.lines{app.selectedlineidx}{2}(1) * magnitude;
-endingYCoord = startingYCoord + app.lines{app.selectedlineidx}{2}(2) * magnitude;
-endingZCoord = startingZCoord + app.lines{app.selectedlineidx}{2}(3) * magnitude;
+startingXCoord = app.lines{app.selectedlineidx}{3}(1)*1e3; % from m to mm
+startingYCoord = app.lines{app.selectedlineidx}{3}(2)*1e3; % from m to mm
+startingZCoord = app.lines{app.selectedlineidx}{3}(3)*1e3; % from m to mm
+magnitude = app.lines{app.selectedlineidx}{2}(4)*1e3; % from m to mm
+xDirection = app.lines{app.selectedlineidx}{2}(1);
+yDirection = app.lines{app.selectedlineidx}{2}(2);
+zDirection = app.lines{app.selectedlineidx}{2}(3);
+
+% get normal components for the direction vector components
+directionVectMag = sqrt(xDirection^2 + yDirection^2 + zDirection^2);
+xNorm = xDirection / directionVectMag;
+yNorm = yDirection / directionVectMag;
+zNorm = zDirection / directionVectMag;
+
+endingXCoord = startingXCoord + xNorm * magnitude;
+endingYCoord = startingYCoord + yNorm * magnitude;
+endingZCoord = startingZCoord + zNorm * magnitude;
 
 %   Plots the line if visability is set to true
 if (app.lines{app.selectedlineidx}{4})
