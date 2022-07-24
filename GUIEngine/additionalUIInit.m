d7d9bbd5582cd237d8837ccabf561fb8e587a524
gornstein <gornstein123@gmail.com>
2022-07-24 11:41:36 -0400

    added stuff to start building planning tab outside of designer

---
+++ GUIEngine/additionalUIInit.m
@@ -0,0 +1,12 @@
+function additionalUIInit(app)
+        % Create ExportingTab
+            app.TestingTab = uitab(app.WindowTabGroup);
+            app.TestingTab.Title = 'Testing';
+            app.TestingTab.BackgroundColor = [0.7608 0.8196 0.7608];
+
+            app.TestingTest = uiaxes(app.TestingTab);
+            title(app.TestingTest, 'Cross Section View')
+            xlabel(app.TestingTest, 'X')
+            ylabel(app.TestingTest, 'Y')
+            zlabel(app.TestingTest, 'Z')
+end
