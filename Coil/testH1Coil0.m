044ba6b4f91277a8c928d7f0ad0ae1c0a4a1caba
saphireraider <96030083+saphireraider@users.noreply.github.com>
2022-08-29 21:09:52 -0400

    Modified the H1 coil to be congruent

--- Coil/testH1Coil0.m
+++ Coil/testH1Coil0.m
@@ -3,8 +3,10 @@ load coil_H1.mat;
 Orig = (max(P)+min(P))/2;
 P = P-Orig;
 strcoil.Pwire = strcoil.Pwire-Orig;
-P = P*1.2;
-strcoil.Pwire = strcoil.Pwire*1.2;
+
+
+% P = P*1.2;
+% strcoil.Pwire = strcoil.Pwire*1.2;
 
 save('coil', 'strcoil');
 save('coilCAD', 'P', 't', 'tind');
